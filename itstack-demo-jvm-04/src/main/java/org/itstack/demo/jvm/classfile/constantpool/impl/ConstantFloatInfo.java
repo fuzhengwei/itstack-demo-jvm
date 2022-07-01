989e3f668da4a8caec941064fe9938b0857ed24b
weipeng001 <34591691+weipeng001@users.noreply.github.com>
2022-07-01 20:36:05 +0800

    Create ConstantFloatInfo.java
    fix: 删除异常的import引用

--- itstack-demo-jvm-04/src/main/java/org/itstack/demo/jvm/classfile/constantpool/impl/ConstantFloatInfo.java
+++ itstack-demo-jvm-04/src/main/java/org/itstack/demo/jvm/classfile/constantpool/impl/ConstantFloatInfo.java
@@ -2,7 +2,6 @@ package org.itstack.demo.jvm.classfile.constantpool.impl;
 
 import org.itstack.demo.jvm.classfile.ClassReader;
 import org.itstack.demo.jvm.classfile.constantpool.ConstantInfo;
-import sun.reflect.ConstantPool;
 
 /**
  * http://www.itstack.org
